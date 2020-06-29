661a8a1f8bd7ffd95e7d09bc90342260e40a749b
aimanjadoon <66253879+aimanjadoon@users.noreply.github.com>
2020-06-29 22:52:05 +0500

    Update Home.java

--- Home.java
+++ Home.java
@@ -437,4 +437,51 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
+	class ReceiveEvents extends Thread{
+			Socket socket= null;
+			Robot robot = null;
+			boolean continueLoop = true;
+
+			public ReceiveEvents(Socket socket, Robot robot){
+
+			this.socket = socket;
+			this.robot = robot;
+			start(); //Start the thread and hence calling run method
+			}
+
+
+
+			public void run(){
+			Scanner scanner = null;
+			try {
+			scanner = new Scanner(socket.getInputStream());
+			while(continueLoop){
+			//recieve commands and respond accordingly
+
+			int command = scanner.nextInt();
+			switch(command){
+					case-1:
+					robot.mousePress(scanner.nextInt());
+					break;
+					case-2:
+					robot.mouseRelease(scanner.nextInt());
+					break;
+					case-3:
+					robot.keyPress(scanner.nextInt());
+					break;
+					case-4:
+					robot.keyRelease(scanner.nextInt());
+					break;
+					case-5:
+					robot.mouseMove(scanner.nextInt(),scanner.nextInt());
+					break;
+					}
+
+					}
+			}catch(IOException ex){
+					ex.printStackTrace();
+					}
+				}//end function
+				
+				}
 }
